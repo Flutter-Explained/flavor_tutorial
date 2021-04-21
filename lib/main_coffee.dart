cabf82cea57f6c7a5849a88372c8bae30ce240db
Max <weber.max.1988@live.de>
2021-04-21 09:58:28 +0200

    Added more TODO

--- lib/main_coffee.dart
+++ lib/main_coffee.dart
@@ -1,24 +1,4 @@
-import 'package:flavour_example/flavor_config.dart';
-import 'package:flavour_example/main_common.dart';
-import 'package:flutter/material.dart';
-
 void main() {
   // Inject our own configurations
   // Coffee Beans
-
-  mainCommon(
-    FlavorConfig()
-      ..appTitle = "Beans Factory"
-      ..apiEndpoint = {
-        Endpoints.items: "random.api.dev/items",
-        Endpoints.details: "random.api.dev/item"
-      }
-      ..imageLocation = "assets/images/coffee_bean.jpg"
-      ..theme = ThemeData.light().copyWith(
-        primaryColor: Color(0xFF123456),
-        appBarTheme: ThemeData.light().appBarTheme.copyWith(
-              backgroundColor: Color(0xFF654321),
-            ),
-      ),
-  );
 }
--- lib/main_cookie.dart
+++ lib/main_cookie.dart
@@ -1,19 +1,6 @@
-import 'package:flavour_example/flavor_config.dart';
-import 'package:flavour_example/main_common.dart';
-import 'package:flutter/material.dart';
-
 void main() {
   // Inject our own configurations
   // Crunchy Munchy
 
-  mainCommon(
-    FlavorConfig()
-      ..appTitle = "Munchy Crunchy"
-      ..imageLocation = "assets/images/munchy_crunchy.jpg"
-      ..apiEndpoint = {
-        Endpoints.items: "api.munchycrunchy.dev/items",
-        Endpoints.details: "api.munchycrunchy.dev/items"
-      }
-      ..theme = ThemeData.dark(),
-  );
+  // TODO: Call the mainCommon function and pass in the cookie Config
 }
